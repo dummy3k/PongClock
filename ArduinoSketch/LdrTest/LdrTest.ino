8ebba329f177ea1f3209cd3116d1fd1259ec657d
dummy3k <l4711@gmx.net>
2017-08-20 22:18:46 +0200

    v1.57

---
+++ ArduinoSketch/LdrTest/LdrTest.ino
@@ -0,0 +1,18 @@
+#define LDR 5
+
+void setup() {
+  Serial.begin(9600);
+  
+  Serial.println(F("LDR Test v0.1"));
+  Serial.println(F(__DATE__));
+  Serial.println(F(__TIME__));
+    
+  pinMode(LDR, INPUT);
+
+}
+
+void loop() {
+   int v = analogRead(LDR);
+   Serial.println(v);
+   delay(1000);
+}
